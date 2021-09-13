524dd79d9c1ea0e1ea7cb938985f5b04bef12cbb
Emily Chuah <emily_earn@live.com>
2021-09-13 18:00:26 +1000

    Programmed the button to display a random image

--- js/script.js
+++ js/script.js
@@ -7,6 +7,25 @@ const getImage = async function () { //The async keyword enables asynchronous co
     const res = await fetch ("https://picsum.photos/v2/list?limit=100"); //The await keyword tells the program to wait on that line in the function until the API data are received.
     const images = await res.json(); //Calling the .json() method on the 'res' response transforms the JSON data into a proper JavaScript object, which can then be used in my code. Since calling .json() on the response is an asynchronous action, use the await keyword.
     console.log(images);
-}
+    selectRandomImage(images);
+};
 
-getImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random()*images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+    getImage(); //This program won't retrieve API data until the button is clicked.
+});
